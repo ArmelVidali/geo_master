25b0e26764af6a35666a3ccc1f21fcd6dc06016f
geo <geo@geo.myguest.virtualbox.org>
2023-09-01 20:50:42 +0200

    added sidebar rendering react

---
+++ geo_master_react/src/sideMenu.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+
+const SideMenu = () => {
+  const [data, setData] = useState(null);
+  var categ = [
+    "administratif",
+    "adresses",
+    "bati",
+    "hydrographie",
+    "lieux_nommes",
+    "occupation_sol",
+    "service_activites",
+    "transports",
+    "zone_reglementes",
+  ];
+
+  useEffect(() => {
+    const fetchAll = async () => {
+      const res = await fetch("http://localhost:3001/all_collections");
+      if (res.ok) {
+        const json = await res.json();
+        setData(json.collections); // Store the data in state
+      }
+    };
+
+    fetchAll();
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {categ.map((category, index) => (
+          <li key={index} id={category}>
+            {category}
+          </li>
+        ))}
+      </ul>
+      {data ? (
+        <ul>
+          {data.map((element, index) => (
+            <li key={index} id={element}>
+              {element
+                .match(/dep_(\d+)/)[0]
+                .replace("dep", "Departement")
+                .replace("_", " ")}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default SideMenu;
